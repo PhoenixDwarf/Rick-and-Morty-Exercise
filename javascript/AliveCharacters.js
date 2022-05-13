d246068d3a0501d546e6873c40c86e916e1d6128
Edwin <95711147+PhoenixDwarf@users.noreply.github.com>
2022-05-12 20:03:21 -0500

    Second commit

---
+++ javascript/AliveCharacters.js
@@ -0,0 +1,41 @@
+const Api_url = 'https://rickandmortyapi.com/api/character/';
+
+window.onload = getAliveCharacters();
+
+function getAliveCharacters(){
+    let male = 0;
+    let female = 0;
+    fetch(Api_url).then(res => res.json()).then(data => {
+        let results = data.results;
+        results.map((data)=>{
+            let article = document.createElement("article");
+            let index = data.id;
+            if(data.status == "Alive"){
+                article.innerHTML = `
+                    <article class="card">
+                        <div class="card-img">
+                            <img src="${data.image}" alt="${data.name}">
+                            <span class="card-img-status" id="${index}">${data.status}</span>
+                        </div>
+                        <div class="card-info">
+                            <span><span class="card-info-bold">Name: </span>${data.name}</span>
+                            <span><span class="card-info-bold">Location: </span>${data.location.name}</span>
+                            <span><span class="card-info-bold">Gender: </span>${data.gender}</span>
+                            <span><span class="card-info-bold">Specie: </span>${data.species}</span>
+                        </div>
+                    </article>
+                `;
+                document.getElementById('cards-section').appendChild(article);
+                document.getElementById(index).style.backgroundColor = "#AEF5B7";
+                if(data.gender == "Male"){
+                    male ++;
+                }
+                else if(data.gender == "Female"){
+                    female ++;
+                }
+                document.getElementById("navbar-right-male").textContent = `${male}` ;
+                document.getElementById("navbar-right-female").innerHTML = `${female}`;
+            }
+        });
+    });
+}
--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,48 @@
-function action(){
-    alert("works");
+const Api_url = 'https://rickandmortyapi.com/api/character/';
+
+window.onload = getAllCharacters();
+
+function getAllCharacters(){
+    let male = 0;
+    let female = 0;
+    fetch(Api_url).then(res => res.json()).then(data => {
+        let results = data.results;
+        results.map((data)=>{
+            let article = document.createElement("article");
+            let index = data.id;
+            article.innerHTML = `
+            <article class="card">
+                <div class="card-img">
+                    <img src="${data.image}" alt="${data.name}">
+                    <span class="card-img-status" id="${index}">${data.status}</span>
+                </div>
+                <div class="card-info">
+                    <span><span class="card-info-bold">Name: </span>${data.name}</span>
+                    <span><span class="card-info-bold">Location: </span>${data.location.name}</span>
+                    <span><span class="card-info-bold">Gender: </span>${data.gender}</span>
+                    <span><span class="card-info-bold">Specie: </span>${data.species}</span>
+                </div>
+            </article>
+            `;
+            document.getElementById('cards-section').appendChild(article);
+            if(data.status == "Alive"){
+                document.getElementById(index).style.backgroundColor = "#AEF5B7";
+            }
+            if(data.status == "Dead"){
+                document.getElementById(index).style.backgroundColor = "#E97E81";
+            }
+            if(data.status == "unknown"){
+                document.getElementById(index).style.backgroundColor = "#C0C0C0";
+            }
+            if(data.gender == "Male"){
+                male ++;
+            }
+            else if(data.gender == "Female"){
+                female ++;
+            }
+            document.getElementById("navbar-right-male").textContent = `${male}` ;
+            document.getElementById("navbar-right-female").innerHTML = `${female}`;
+        });
+    });
+    
 }
